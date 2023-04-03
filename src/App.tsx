3dc4ea0d11c68d6f47205af82087cf425e6333de
SrikanthBethi9885 <113573467+SrikanthBethi9885@users.noreply.github.com>
2023-04-03 11:05:51 +0000

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import { Card } from '@mui/material';
 
 function App() {
   return (
-<div><h2>Hello World</h2></div>      
+<Card sx={{height:'100px', width:'150px',alignItems:'center',backgroundColor:'#456631',color:'#EC6613',padding:'25px 32px 40px'}}><h2>Hello World</h2></Card>      
   );
 }
 
