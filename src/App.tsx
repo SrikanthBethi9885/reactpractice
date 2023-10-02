f3ae2bdc5418e9afe40375e4c968cf8b104228a2
SrikanthBethi9885 <srikanth.bethi@missiong3.com>
2023-10-02 21:15:29 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <h2>
           I am Srikanth Bethi</h2>
         <h2>
-          I am Srikanth Hi how are you</h2>
+          I am Srikanth Hi how are you is it ok</h2>
       </div>
     </>
   );
