fc476d865d20b9219e6d59a654d8e7e3e526951a
SrikanthBethi9885 <srikanth.bethi@missiong3.com>
2023-10-02 14:06:25 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import { Card } from '@mui/material';
 
 function App() {
   return (
-<Card sx={{height:'100px', width:'150px',alignItems:'center',backgroundColor:'#456631',color:'#EC6613',padding:'25px 32px 40px'}}><h2>Hello World</h2></Card>      
+    <>
+      <div style={{ display: 'flex', justifyContent: 'center' }}><Card sx={{ height: '100px', width: '150px', alignItems: 'center', backgroundColor: '#456631', color: '#EC6613', padding: '25px 32px 40px' }}><h2>Hello World</h2></Card>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <h2>
+          I am Srikanth Bethi</h2>
+      </div>
+    </>
   );
 }
 
